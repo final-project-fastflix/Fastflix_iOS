db5e716e2ac86968647f80dcfd568152eb3743bf
Weonpyo Hong <wp1004@hanmail.net>
2019-08-02 02:40:22 +0900

    feat: allen - UserIconSelectVC

---
+++ Fastflix/Fastflix/Login/LoginController/UserIconSelectVC.swift
@@ -0,0 +1,37 @@
+//
+//  UserIconSelectVC.swift
+//  Fastflix
+//
+//  Created by HongWeonpyo on 02/08/2019.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import UIKit
+
+class UserIconSelectVC: UIViewController {
+  
+  let tableView: UITableView = {
+      let tableView = UITableView()
+    
+    
+      return tableView
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
