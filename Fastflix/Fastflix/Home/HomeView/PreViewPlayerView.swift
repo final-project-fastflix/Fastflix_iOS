6b3c4789da4e7f917f61cbcd9e801f987de477b2
Jeon-heaji <heajijeon@gmail.com>
2019-08-05 17:25:30 +0900

    feat: devy - LogoView, PreViewPlayerView

---
+++ Fastflix/Fastflix/Home/HomeView/PreViewPlayerView.swift
@@ -0,0 +1,21 @@
+//
+//  PreViewPlayerView.swift
+//  Fastflix
+//
+//  Created by Jeon-heaji on 05/08/2019.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import UIKit
+
+class PreViewPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
