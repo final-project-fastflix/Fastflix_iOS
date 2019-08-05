2e3c68adff5f7126681db8febb5148e97cfe8750
Jeon-heaji <heajijeon@gmail.com>
2019-08-05 16:52:00 +0900

    feat: devy - PreviewPlayerCollection

---
+++ Fastflix/Fastflix/Home/HomeCell/PreviewPlayerCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  PreviewPlayerCollectionCell.swift
+//  Fastflix
+//
+//  Created by Jeon-heaji on 05/08/2019.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import UIKit
+
+class PreviewPlayerCollectionCell: UICollectionViewCell {
+    
+}
