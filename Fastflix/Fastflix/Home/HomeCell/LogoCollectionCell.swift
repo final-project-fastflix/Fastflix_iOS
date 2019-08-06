743f8f1a064d99268d29522afda8746ebb3b659f
Jeon-heaji <heajijeon@gmail.com>
2019-08-06 15:22:19 +0900

    feat: devy - PlayCollectionViewCell

--- Fastflix/Fastflix/Home/HomeCell/LogoCollectionCell.swift
+++ Fastflix/Fastflix/Home/HomeCell/LogoCollectionCell.swift
@@ -9,16 +9,23 @@
 import UIKit
 import SnapKit
 
+protocol LogoCollectionCellDelegate {
+  func dismissBtnDidTap()
+}
 
 class LogoCollectionCell: UICollectionViewCell {
-    static let identifier = "WatchingMoviesCollectionCell"
   
-  let logoLabel: UILabel = {
-    let label = UILabel()
-    return label
+  static let identifier = "LogoCollectionCell"
+  
+  var delegate: LogoCollectionCellDelegate?
+  
+  var logoImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = UIImage(named: "preViewLogo")
+    return imageView
   }()
   
-  let progressBar: UISlider = {
+  var progressBar: UISlider = {
     let slider = UISlider()
     slider.tintColor = .white
     slider.thumbTintColor = .clear
@@ -26,22 +33,49 @@ class LogoCollectionCell: UICollectionViewCell {
     
   }()
   
+  private let dismissBtn: UIButton = {
+    let button = UIButton(type: .custom)
+    button.addTarget(self, action: #selector(dismissBtnDidTap(_:)), for: .touchUpInside)
+    button.setImage(UIImage(named: "x"), for: .normal)
+    return button
+  }()
+  
   override func didMoveToSuperview() {
     super.didMoveToSuperview()
+    self.backgroundColor = .clear
     addSubViews()
     setupSNP()
     
   }
   
   private func addSubViews() {
-    [logoLabel, progressBar].forEach {
+    [logoImageView, progressBar, dismissBtn].forEach {
       self.addSubview($0)
     }
   }
   
   private func setupSNP() {
+    progressBar.snp.makeConstraints {
+      $0.top.equalToSuperview()
+      
+    }
+    
+    logoImageView.snp.makeConstraints {
+      $0.top.equalTo(progressBar.snp.bottom).offset(10)
+      $0.width.height.equalTo(200)
+    }
+    
+    dismissBtn.snp.makeConstraints {
+      $0.top.equalToSuperview()
+      $0.trailing.equalToSuperview().inset(10)
+    }
+  }
+  
+  @objc private func dismissBtnDidTap(_ sender: UIButton) {
+    print(" 뒤로가여 ")
+    delegate?.dismissBtnDidTap()
     
   }
   
-
+  
 }
--- Fastflix/Fastflix/Home/HomeView/PreViewPlayerView.swift
+++ Fastflix/Fastflix/Home/HomeView/PreViewPlayerView.swift
@@ -24,10 +24,35 @@ class PreViewPlayerView: UIView {
     return collectionView
   }()
   
+  private lazy var pokeButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setImage(UIImage(named: "add"), for: .normal)
+    button.setImage(UIImage(named: "poke"), for: .selected)
+    button.addTarget(self, action: #selector(pokeBtnDidTap(_:)), for: .touchUpInside)
+    return button
+  }()
+  
+  private let playButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setImage(UIImage(named: "play2"), for: .normal)
+    return button
+  }()
+  
+  private let infoButton: UIButton = {
+    let button = UIButton(type: .custom)
+    button.setImage(UIImage(named: "info"), for: .normal)
+    button.tintColor = .white
+    return button
+  }()
+  
+  
   lazy var stackView: UIStackView = {
     let stackView = UIStackView()
     return stackView
   }()
+  
+  let img = ["test1", "test2", "test3", "test4"]
+  
 
   override func didMoveToSuperview() {
     super.didMoveToSuperview()
@@ -41,14 +66,27 @@ class PreViewPlayerView: UIView {
   
   private func addSubViews() {
     
-    [logoCollectionView, playCollectionView]
-      .forEach{ self.addSubview($0) }
+    [logoCollectionView, playCollectionView, stackView].forEach {
+      self.addSubview($0)
+    }
     
   }
   private func setupSNP() {
     logoCollectionView.snp.makeConstraints {
+      $0.top.leading.trailing.equalToSuperview()
+    }
+    
+    playCollectionView.snp.makeConstraints {
       $0.top.leading.trailing.bottom.equalToSuperview()
     }
+    
+    stackView.snp.makeConstraints {
+      $0.bottom.equalToSuperview().offset(-20)
+      $0.centerX.equalToSuperview()
+    }
+    
+    
+    
     
   }
   private func setupCollectionView() {
@@ -58,11 +96,48 @@ class PreViewPlayerView: UIView {
     playCollectionView.dataSource = self
     playCollectionView.delegate = self
     
+    layout.scrollDirection = .horizontal
+    self.logoCollectionView.collectionViewLayout = layout
+    self.playCollectionView.collectionViewLayout = layout
+    layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    
+    layout.minimumLineSpacing = 0
+
+    layout.minimumInteritemSpacing = 0
+    
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    
+    layout.itemSize = CGSize(width: width, height: height)
+    
+    layout.sectionHeadersPinToVisibleBounds = true
+    playCollectionView.showsHorizontalScrollIndicator = false
     
   }
   
+  private func setupStackView() {
+    // stackView
+    stackView = UIStackView(arrangedSubviews: [pokeButton, playButton, infoButton])
+    stackView.axis = .horizontal
+    stackView.distribution = .fillEqually
+    stackView.spacing = 50
+
+  }
+  @objc func pokeBtnDidTap(_ sender: UIButton) {
+    UIView.animate(withDuration: 0.3) {
+      self.pokeButton.alpha = 0
+    }
+    sender.isSelected.toggle()
+    UIView.animate(withDuration: 0.3) {
+      self.pokeButton.alpha = 1
+    }
+  }
+  
+  
+  
   private func registerCollectionCell() {
     logoCollectionView.register(LogoCollectionCell.self, forCellWithReuseIdentifier: LogoCollectionCell.identifier)
+    playCollectionView.register(PlayCollectionViewCell.self, forCellWithReuseIdentifier: PlayCollectionViewCell.identifier)
 
   }
 
@@ -70,13 +145,33 @@ class PreViewPlayerView: UIView {
 
 extension PreViewPlayerView : UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 1
+    if collectionView == self.logoCollectionView {
+      return 1
+    } else {
+      return img.count
+    }
+    
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PlayCollectionCell.identifier, for: indexPath) as! PlayCollectionCell
+    let cell = UICollectionViewCell()
+    
+    if collectionView == self.logoCollectionView {
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LogoCollectionCell.identifier, for: indexPath) as! LogoCollectionCell
+      cell.logoImageView.image = UIImage(named: "preViewLogo")
+      
+      return cell
+      
+    } else if collectionView == self.playCollectionView {
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PlayCollectionViewCell.identifier, for: indexPath) as! PlayCollectionViewCell
+
+      cell.playView.image = UIImage(named: img[indexPath.row])
+      return cell
+    }
+    
     return cell
+  
   }
 }
 
