f08b8cb25865435655f63b82d0be21cbf90227a4
hyeoktae kwon <kht_0218@hanmail.net>
2019-08-15 02:15:16 +0900

    feat: tass - test preview

---
+++ TestPreview/TestPreview/MainController.swift
@@ -0,0 +1,17 @@
+//
+//  MainController.swift
+//  TestPreview
+//
+//  Created by hyeoktae kwon on 2019/08/15.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+}
