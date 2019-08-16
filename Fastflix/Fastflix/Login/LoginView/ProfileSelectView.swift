a98a0bcac38b04155e3755309ad31e5994bb1ecf
Weonpyo Hong <wp1004@hanmail.net>
2019-08-16 19:58:02 +0900

    feat: allen - separate view and vc for ProfileSelectVC.

---
+++ Fastflix/Fastflix/Login/LoginView/ProfileSelectView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileSelectView.swift
+//  Fastflix
+//
+//  Created by HongWeonpyo on 16/08/2019.
+//  Copyright © 2019 hyeoktae kwon. All rights reserved.
+//
+
+import UIKit
+
+class ProfileSelectView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
